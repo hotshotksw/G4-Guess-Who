1eaecd5fea6ad3413f48486f8a9f197a34a26973
SKimberlin <sethkimberlin04@gmail.com>
2025-01-24 12:14:31 -0700

    Added Camera Movement

--- flipper_image.gd
+++ flipper_image.gd
@@ -21,13 +21,17 @@ func _process(delta: float) -> void:
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if open:
-				anim_player.play('Flip')
-				print('Close')
-			else:
-				anim_player.play_backwards('Flip')
-				print('Open')
-			open = !open
-		elif event.button_index == MOUSE_BUTTON_MASK_RIGHT and event.pressed and open:
-			print('Are you sure you want to guess them?')
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				if open:
+					anim_player.play('Flip')
+					print('Close')
+
+				else:
+					anim_player.play_backwards('Flip')
+					print('Open')
+
+				open = !open
+
+			elif event.button_index == MOUSE_BUTTON_MASK_RIGHT and open:
+				print('Are you sure you want to guess them?')
--- player.gd
+++ player.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
-@onready var camera = $Camera3D as Camera3D
+@onready var camera = $MeshInstance3D/Boy/Camera3D as Camera3D
+@onready var head = $MeshInstance3D/Boy
+@onready var ray = $MeshInstance3D/Boy/Camera3D/RayCast3D as RayCast3D
+
+var x_sensitivity = 0.01 as float
+var y_sensitivity = 0.01 as float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,12 +13,42 @@ func _ready() -> void:
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
 		camera.make_current()
 	
-	pass # Replace with function body.
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input(event: InputEvent) -> void:
+	if $MultiplayerSynchronizer.get_multiplayer_authority() != multiplayer.get_unique_id():
+		return
+
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			head.rotate_y(-event.relative.x * x_sensitivity)
+			head.rotation.x = clamp(head.rotation.x + event.relative.y * y_sensitivity, 
+				deg_to_rad(-30), deg_to_rad(90))
+
+		elif event is InputEventMouseButton and event.pressed:
+			if ray.is_colliding():
+				var node = ray.get_collider()
+	
+				while node and not node.has_method("_on_area_3d_input_event"):
+					node = node.get_parent()
+	
+				if node:
+					var custom_event = InputEventMouseButton.new()
+					custom_event.button_index = event.button_index
+					custom_event.pressed = true
+					node._on_area_3d_input_event(camera, custom_event, ray.get_collision_point(),
+						ray.get_collision_normal(), ray.get_collider_shape())
+	
+		elif event.is_action_pressed("ui_cancel"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	elif Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		if event is InputEventMouseButton and event.pressed:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id()
 # -- if true, following code will occur
