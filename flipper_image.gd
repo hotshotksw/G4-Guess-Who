b6a6bbdbfaafb9ca60aa0b36c276d53ab5992b99
Seth Kimberlin <sethkimberlin04@gmail.com>
2025-01-23 19:27:25 -0700

    Updated Flipper

--- flipper_image.gd
+++ flipper_image.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @export var texture = preload("res://models/wpwh1qb3.jpg")
+var open = true
+@onready var anim_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +17,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if open:
+				anim_player.play('Flip')
+				print('Close')
+			else:
+				anim_player.play_backwards('Flip')
+				print('Open')
+			open = !open
+		elif event.button_index == MOUSE_BUTTON_MASK_RIGHT and event.pressed and open:
+			print('Are you sure you want to guess them?')
